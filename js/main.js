cb75aa2d997c9229e0c6dbf07369f44f8699e53e
Tadas Valantonis <valantonis1@gmail.com>
2023-11-01 17:17:41 +0200

    Added "see more", and few other changes

--- js/main.js
+++ js/main.js
@@ -1,3 +1,46 @@
+// Collab Example Section
+const vendorSection = document.querySelector('.collab-example-section .vendor');
+const supervisorSection = document.querySelector('.collab-example-section .supervisor');
+const vendorButton = document.querySelector('.collab-example-section .vendor-button');
+const supervisorButton = document.querySelector('.collab-example-section .supervisor-button');
+
+vendorButton.addEventListener('click', () => {
+	if (vendorSection.classList.contains('ce-inactive-section')) {
+		supervisorSection.classList.add('ce-inactive-section');
+		vendorSection.classList.remove('ce-inactive-section');
+		vendorButton.classList.remove('ce-inactive-button');
+		supervisorButton.classList.add('ce-inactive-button');
+	}
+});
+
+supervisorButton.addEventListener('click', () => {
+	if (supervisorSection.classList.contains('ce-inactive-section')) {
+		vendorSection.classList.add('ce-inactive-section');
+		supervisorSection.classList.remove('ce-inactive-section');
+		supervisorButton.classList.remove('ce-inactive-button');
+		vendorButton.classList.add('ce-inactive-button');
+	}
+});
+
+// Collab Examples Show More/Less Text
+const dots = document.querySelectorAll('.collab-example-section .vendor li > .dots,.supervisor li > .dots');
+const moreText = document.querySelectorAll('.collab-example-section .vendor li > .text-more,.supervisor li > .text-more');
+const buttonText = document.querySelectorAll('.collab-example-section .vendor li > .see-more,.supervisor li > .see-more');
+
+buttonText.forEach((button, i) => {
+	button.addEventListener('click', function() {
+		if (dots[i].style.display === "none") {
+			dots[i].style.display = "inline";
+			button.innerHTML = "see more";
+			moreText[i].style.display = "none";
+		} else {
+			dots[i].style.display = "none";
+			button.innerHTML = "see less";
+			moreText[i].style.display = "inline";
+		}
+	});
+});
+
 // Feedback Carouselle
 const carousel = new Flickity( '.feedback-carousel', {
 	cellAlign: "left",
@@ -49,28 +92,4 @@ checkboxElement.addEventListener('change', function() {
 		monthlyElement.style.color = '#000';
 		yearlyElement.style.color = '#A79D9D';
 	  }
-});
-
-// Collab Example Section
-const vendorSection = document.querySelector('.collab-example-section .vendor');
-const supervisorSection = document.querySelector('.collab-example-section .supervisor');
-const vendorButton = document.querySelector('.collab-example-section .vendor-button');
-const supervisorButton = document.querySelector('.collab-example-section .supervisor-button');
-
-vendorButton.addEventListener('click', () => {
-	if (vendorSection.classList.contains('ce-inactive-section')) {
-		supervisorSection.classList.add('ce-inactive-section');
-		vendorSection.classList.remove('ce-inactive-section');
-		vendorButton.classList.remove('ce-inactive-button');
-		supervisorButton.classList.add('ce-inactive-button');
-	}
-});
-
-supervisorButton.addEventListener('click', () => {
-	if (supervisorSection.classList.contains('ce-inactive-section')) {
-		vendorSection.classList.add('ce-inactive-section');
-		supervisorSection.classList.remove('ce-inactive-section');
-		supervisorButton.classList.remove('ce-inactive-button');
-		vendorButton.classList.add('ce-inactive-button');
-	}
 });
